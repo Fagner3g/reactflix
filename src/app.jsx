c1bffae2390c700122a9f3652df060bd17c6b5d2
Fagner Gomesfag <fagner.egomes@hotmail.com>
2018-08-13 16:26:42 -0300

    Aula 73 - Styled Lista de Video

--- src/app.jsx
+++ src/app.jsx
@@ -3,6 +3,8 @@ import styled, { injectGlobal } from 'styled-components'
 import 'milligram'
 import 'normalize.css'
 
+import Play from './components/play'
+import play from './components/play';
 const App = () => (
     <Container>
         <Header>
@@ -10,7 +12,16 @@ const App = () => (
         </Header>
 
         <Main>
-            Conteúdo
+            <VideosList>
+                {Array.from({ length: 10 }).map((item, index) => (
+                    <Video key={index}>
+                        <VideoThumb>
+                            <PlayStyled />
+                        </VideoThumb>
+                        <VideoTitle>Titulo do Video</VideoTitle>
+                    </Video>
+                ))}
+            </VideosList>
         </Main>
 
         <Footer>
@@ -43,6 +54,29 @@ const Main = styled.main`
 const Footer = styled.footer`
     height: ${footerHeight};
     background: #333;
+`
+const VideosList = styled.div`
+    Display: flex;
+    flex-wrap: wrap
+`
+const Video = styled.section`
+ flex: 1 1 300px;
+ margin: 0 5px 5px;
+`
+const PlayStyled = styled(Play)`
+    fill: #999;
+    height: 50px;
+    width: 50px;
+`
+const VideoThumb = styled.div`
+    align-items: center;
+    border: 1px solid #999;
+    display: flex;
+    height: 150px;
+    justify-content: center;
+`
+const VideoTitle = styled.h2`
+    font-size: 18px;
 `
 
 export default App
