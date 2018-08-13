4252645efdee26bd3dded57632ab2d82d385a2cc
Fagner Gomesfag <fagner.egomes@hotmail.com>
2018-08-13 15:39:20 -0300

    Aula - 73 Estrutura base da aplicação

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,48 @@
 import React from 'react'
+import styled, { injectGlobal } from 'styled-components'
 import 'milligram'
 import 'normalize.css'
 
 const App = () => (
-    <div style={{ display: 'flex', justifyContent: 'space-around', padding: 30 }}>
-        <h1>Ambiente de desenvolvimento padrão</h1>
-    </div>
+    <Container>
+        <Header>
+            <h1 style={{ margin: 0 }}>Reactflix</h1>
+        </Header>
+
+        <Main>
+            Conteúdo
+        </Main>
+
+        <Footer>
+            &copy;2018
+        </Footer>
+    </Container>
 )
+
+injectGlobal`
+    html, body, #app {
+        height: 100%;
+    }
+`
+
+const headerHeight = '60px'
+const footerHeight = '30px'
+
+const Container = styled.div`
+    height: 100%;
+`
+
+const Header = styled.header`
+    height: ${headerHeight};
+    background: #333;
+`
+const Main = styled.main`
+    min-height: calc(100% - ${headerHeight} - ${footerHeight});
+`
+
+const Footer = styled.footer`
+    height: ${footerHeight};
+    background: #333;
+`
 
 export default App
