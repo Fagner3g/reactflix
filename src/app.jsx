ba3711d87ee6e69cbf7a1782acc9779bff690565
Fagner Gomesfag <fagner.egomes@hotmail.com>
2018-08-13 16:55:57 -0300

    Aula 74 - Melhorando estilo do componente de vídeo

--- src/app.jsx
+++ src/app.jsx
@@ -3,8 +3,7 @@ import styled, { injectGlobal } from 'styled-components'
 import 'milligram'
 import 'normalize.css'
 
-import Play from './components/play'
-import play from './components/play';
+import VideosList from './components/videos-list/index'
 const App = () => (
     <Container>
         <Header>
@@ -12,16 +11,7 @@ const App = () => (
         </Header>
 
         <Main>
-            <VideosList>
-                {Array.from({ length: 10 }).map((item, index) => (
-                    <Video key={index}>
-                        <VideoThumb>
-                            <PlayStyled />
-                        </VideoThumb>
-                        <VideoTitle>Titulo do Video</VideoTitle>
-                    </Video>
-                ))}
-            </VideosList>
+            <VideosList />
         </Main>
 
         <Footer>
@@ -54,29 +44,6 @@ const Main = styled.main`
 const Footer = styled.footer`
     height: ${footerHeight};
     background: #333;
-`
-const VideosList = styled.div`
-    Display: flex;
-    flex-wrap: wrap
-`
-const Video = styled.section`
- flex: 1 1 300px;
- margin: 0 5px 5px;
-`
-const PlayStyled = styled(Play)`
-    fill: #999;
-    height: 50px;
-    width: 50px;
-`
-const VideoThumb = styled.div`
-    align-items: center;
-    border: 1px solid #999;
-    display: flex;
-    height: 150px;
-    justify-content: center;
-`
-const VideoTitle = styled.h2`
-    font-size: 18px;
 `
 
 export default App
