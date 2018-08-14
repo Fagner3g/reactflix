6f3902e772b022d4257f7969092f49758ad55b91
Fagner Gomesfag <fagner.egomes@hotmail.com>
2018-08-14 08:51:03 -0300

    Aula 77 - Lendo dados do firebase

--- src/index.jsx
+++ src/index.jsx
@@ -7,6 +7,18 @@ import configureStore from './redux-flow/configure-store'
 
 const INITIAL_STATE = {}
 
+const db = firebase.database()
+const videos = db.ref('videos')
+
+// videos.on('value', (snapshot) => {
+//     console.log('snapshot: ', snapshot.val())
+// }, (erro) => {
+//     console.log('Error: ', erro)
+// })
+videos.once('value').then((snapshot) => {
+    console.log('Snapshot:', snapshot.val())
+})
+
 const store = configureStore(INITIAL_STATE)
 
 ReactDOM.render(
