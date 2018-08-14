51c5645a7af910f6856b9090f51cd404f94d5a38
Fagner Gomesfag <fagner.egomes@hotmail.com>
2018-08-14 15:52:13 -0300

    Aula 88 - extrair Header e Footer do App

--- src/app.jsx
+++ src/app.jsx
@@ -6,23 +6,19 @@ import 'milligram'
 import VideosList from './components/videos-list'
 import VideoSingle from './components/video-single'
 import RegisterVideo from './components/register-video'
-
+import Header from './components/header'
+import { headerHeight, footerHeight } from './utils/constants';
+import Footer from './components/footer'
 const App = () => (
     <Container>
-        <Header>
-            <HeaderTitle>Reactflix</HeaderTitle>
-            <RegisterButton>Cadastrar vídeo </RegisterButton>
-        </Header>
-
+        <Header />
         <Main>
             <RegisterVideo />
             <VideoSingle />
             <VideosList />
         </Main>
 
-        <Footer>
-            &copy;2018
-        </Footer>
+        <Footer />
     </Container>
 )
 
@@ -31,37 +27,13 @@ injectGlobal`
         height: 100%;
     }
 `
-
-const headerHeight = '60px'
-const footerHeight = '30px'
-
 const Container = styled.div`
     height: 100%;
 `
-
-const Header = styled.header`
-    align-items: center;
-    background: #333;
-    display: flex;
-    height: ${headerHeight};
-    justify-content: space-between;
-    padding: 0 10px;
-`
-const RegisterButton = styled.button`
-    margin-bottom: 0;
-`
-const HeaderTitle = styled.h1`
-    color: #fff;
-    margin-bottom: 0;
-`
-
 const Main = styled.main`
     min-height: calc(100% - ${headerHeight} - ${footerHeight});
 `
 
-const Footer = styled.footer`
-    height: ${footerHeight};
-    background: #333;
-`
+
 
 export default App
